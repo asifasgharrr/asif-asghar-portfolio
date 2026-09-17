cd6856b1015d27659988ca6ff0e74b569403c15a
asifasgharrr <aassigfhar@gmail.com>
2026-09-17 18:40:05 +0500

    Delete asif_portfolio_v13_qa_hardened directory
    Remove obsolete V13

--- asif_portfolio_v13_qa_hardened/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist .venv\Scripts\python.exe (
-  echo Creating virtual environment...
-  py -m venv .venv
-)
-call .venv\Scripts\activate.bat
-python -m pip install -r requirements.txt
-python app.py
